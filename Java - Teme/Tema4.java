6d7f6f4a4e8f1796e3bafaaeaf42ec6ffd5c0664
Marian Puiu <marian.puiu@tremend.com>
2017-10-27 16:05:52 +0300

    Tema2

---
+++ Java - Teme/Tema4.java
@@ -0,0 +1,14 @@
+public class Tema4 {
+    public static void main (String[] args){
+        int a = 2;
+        int b = 3;
+        /*
+        System.out.println("Primul Operator: ");
+        System.out.println(a);
+        System.out.println("Al doilea Operator: ");
+        System.out.println(b);
+        System.out.println("Sum: ");
+         */
+        System.out.println(a+b);
+    }
+}
