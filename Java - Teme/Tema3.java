47931a6fd7bbb1d88ae7b70a15d29ce562382837
Marian Puiu <marian.puiu@tremend.com>
2017-10-27 16:00:59 +0300

    Tema3

---
+++ Java - Teme/Tema3.java
@@ -0,0 +1,14 @@
+public class Tema3 {
+    public static void main(String[] args) {
+        /**operator unu*/
+        int operand1 = 2;
+        /**operator doi*/
+        int operand2 = 3;
+        /**suma operatorilor*/
+        int suma = (operand1 + operand2);
+        /**afisare suma*/
+        System.out.println(suma);
+
+    }
+}
+
