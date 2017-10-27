e125164e82796635f520fb3962061d1ebf378166
Marian Puiu <marian.puiu@tremend.com>
2017-10-27 16:23:58 +0300

    Tema5

---
+++ Java - Teme/Tema5.java
@@ -0,0 +1,9 @@
+public class Tema5 {
+    public static void main(String[] args){
+        int years;
+        double salary;
+        double x = 15.757;
+        int y = (int)x;
+        System.out.println(y);
+    }
+}
