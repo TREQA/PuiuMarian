ba255705ed069c628e1d9199ac0fe59b1f76ae9f
Marian Puiu <marian.puiu@tremend.com>
2017-11-01 13:27:34 +0200

    Tema12

---
+++ Java - Teme/Tema11.java
@@ -0,0 +1,9 @@
+// daca x primeste o valoare mai mare decat zero, se intra in blocul de cod si x este egal cu 3
+public class Tema11 {
+    public static void main(String[] args){
+        int x = 2;
+        if (x>0){
+            x=3;
+        }
+    }
+}
---
+++ Java - Teme/Tema12.java
@@ -0,0 +1,21 @@
+// cod modificat pt a fi corect
+public class Tema12 {
+    public static void main(String[] args) {
+        if (x==y){}
+        else if(x==0){}
+        else{}
+            }
+        }
+
+        /*
+        Raspunsuri intrebari:
+        1. a
+        2. a
+        3. a
+        4. a
+        5. a
+        6. a
+        7. a
+         */
+
+
