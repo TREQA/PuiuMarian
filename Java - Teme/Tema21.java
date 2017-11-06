dce7e9fd78d4c4aebadb26abda48e60634529b0e
Marian Puiu <marian.puiu@tremend.com>
2017-11-06 14:12:22 +0200

    Tema22

--- Java - Teme/Tema21.java
+++ Java - Teme/Tema21.java
@@ -5,5 +5,54 @@ A treia clasa se numeste Calc si contine metoda statica area care, ca parametru,
 (Circle sau Rectangle), iar ca rezultat prezinta aria formei introduse. In functie daca in metoda a intrat instanta clasei Circle sau Rectangle,
 va fi exeutata formula de calculare a ariei pentru Circle sau Rectangle.
  */
+
 public class Tema21 {
+    public static void main(String[] args) {
+
+        Circle k = new Circle(4);
+        System.out.println(Calculare.area(k));
+        Rectangle r = new Rectangle(2, 3);
+        System.out.println(Calculare.area(r));
+    }
+
+    public static class Circle {
+        public double r;
+
+        public Circle(double r) {
+            this.r = r;
+        }
+    }
+
+    public static class Rectangle {
+        public double a;
+        public double b;
+
+        public Rectangle(double a, double b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    public static class Calculare {
+        public static double area(Object shape) {
+            if (shape.getClass() == Circle.class) {
+                return Math.pow(((Circle) shape).r, 2) * Math.PI;
+            }
+            if (shape.getClass() == Rectangle.class) {
+                return ((Rectangle) shape).a * ((Rectangle) shape).b;
+            }
+            return 0;
+        }
+    }
 }
+
+/*
+Raspunsuri intrebari:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+ */
---
+++ Java - Teme/Tema22.java
@@ -0,0 +1,44 @@
+/*
+Trebuie creata o clasa Shape care va contine unele datea referitoare la forma geometrica, precum: pozitia (x,y), culoarea si denumirea.
+Trebuie create trei clase care vor mosteni clasa Shape. O clasa trebuie creata pentru cerc, una pentru patrat si una pentru dreptunghi.
+Fiecare trebuie sa contina o metoda pentru calcularea ariei, precum si propriile atribute necesare pentru aceasta calculare
+(laturile pentru dreptunghi si patrat si raza pentru cerc). De asemenea, cercul trebuie sa posede si constanta PI.
+Toate valorile vor fi intregi(dar nu si PI).
+ */
+
+public class Tema22 {
+
+    public static void main(String[]args){
+        Circle c = new Circle();
+        c.r = 12;
+        c.color = "crvena";
+        c.x=100;
+        c.y=200;
+        System.out.println(c.area());
+    }
+
+        public String color;
+        public int x;
+        public int y;
+
+    public static class Rectangle extends Tema22{
+        public double a;
+        public double b;
+        public double area(){
+            return a*b;
+        }
+    }
+    public static class Square extends Tema22{
+        public double a;
+        public double area() {
+            return a*a;
+        }
+    }
+    public static class Circle extends Tema22{
+        public final double PI = 3.14;
+        public double r;
+        public double area(){
+            return r*r*PI;
+        }
+    }
+}
