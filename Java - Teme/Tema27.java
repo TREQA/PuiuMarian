0c8b2498b749cd944ba6a9d83290307b2f5f8782
Marian Puiu <marian.puiu@tremend.com>
2017-11-06 17:19:10 +0200

    Tema27

--- Java - Teme/Tema27.java
+++ Java - Teme/Tema27.java
@@ -22,3 +22,14 @@ public class Tema27 implements Tema26, Tema25 {
         System.out.println(c.sub());
     }
 }
+
+/*
+Raspunsuri intrebari:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+ */
---
+++ Java - Teme/Tema28.java
@@ -0,0 +1,10 @@
+/*
+Raspunsuri intrebari:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+ */
