f48a1edf4ba9caab34820573be75231d10ab1ee4
Marian Puiu <marian.puiu@tremend.com>
2017-11-06 10:39:53 +0200

    Tema19

---
+++ Java - Teme/Tema19.kt
@@ -0,0 +1,29 @@
+// trebuie creata clasa Animal care va fi definita ca una abstracta, iar apoi trebuie create doua clase
+// Horse si Dog care vor mosteni prima clasa Animal
+
+public abstract class Animal{
+
+}
+class Horse extends Animal{
+
+}
+
+class Dog extends Animal{
+
+}
+
+// trebuie executata instantierea claselor din exercitiul precedent
+
+Horse horse = new Horse();
+Dog dog = new Dog();
+
+/*
+Raspunsuri intrebari:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+*/
