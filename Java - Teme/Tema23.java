a17c6630db35eb26ec3cb779353964b3dca62819
Marian Puiu <marian.puiu@tremend.com>
2017-11-06 16:43:52 +0200

    Tema22

---
+++ Java - Teme/Tema23.java
@@ -0,0 +1,32 @@
+/*
+trebuie creata o clasa abstracta Quad care va contine 3 campuri (x, y si a) si o metoda abstracta (Setxya)
+trebuie creata interfata IGeom care va contine semnaturile metodelor pentru calcularea ariei si perimetrului
+trebuie creata clasa Square care mosteneste clasa Quad si implementeaza interfata IGeom
+trebuie creata o isntanta a clasei Square in programul principal
+ */
+interface Tema23 {
+    int arae();
+    int perimetru();
+}
+abstract class Quad{
+    public int x;
+    public int y;
+    public int a;
+    public abstract void setxya(int x, int y, int a);
+}
+class Square extends Quad implements Tema23{
+    @Override
+    public int area(){
+        return a*a;
+    }
+    @Override
+    public int perimeter(){
+        return 4*this.a;
+    }
+    @Override
+    public void setxya(int x, int y, int a){
+        this.x=x;
+        this.y=y;
+        this.a=a;
+    }
+}
---
+++ Java - Teme/Tema24.java
@@ -0,0 +1,10 @@
+//continuare tema23
+
+public class Tema24{
+    public static void main (String[]args){
+        Square s = new Square();
+        s.setxya(100,100,5);
+        System.out.println(s.area());
+        System.out.println(s.perimeter());
+    }
+}
